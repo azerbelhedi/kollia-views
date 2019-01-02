67c15b7fa83da8ae7462d22add84cada2ddb9a37
azerbelhedi <azerbelhedide@gmail.com>
2019-01-02 12:13:47 +0100

    day to day visits and duratino

--- visits.js
+++ visits.js
@@ -8,16 +8,34 @@ const errVisits = (err) => {
 } 
 
 const gotVisits = (data) =>{
+    let todayVisits = 0 ; 
+    let todayDuration = 0 ;
     fullDuration = 0 ;
     data = data.val() ;
     let keys = Object.keys(data) ;
     //alert(keys.length) ;
-    document.querySelector("#visits").innerHTML = "all visits : " + keys.length ;
+    document.querySelector("#visits").innerHTML = "visits : " + keys.length ;
     keys.map(x => {
         console.log(data[x].duration) ;
         fullDuration += data[x].duration ;
     });
     document.querySelector("#duration").innerHTML = "duration : " + fullDuration + " min" ; 
+
+    //  daily visits and duration
+    let today = new Date() ;
+    let todayYear = today.getFullYear() ;
+    let todayMonth = today.getMonth() + 1 ;
+    let todayDay = today.getDate() ;
+    keys.map(x => {
+        console.log(data[x].date.day) ;
+        if(todayYear == data[x].date.year && todayMonth == data[x].date.month && todayDay == data[x].date.day ){
+            todayVisits++ ;
+            todayDuration += data[x].duration ;
+        }
+    });
+    document.querySelector("#visits2").innerHTML = "today visits : " + todayVisits ;
+    document.querySelector("#duration2").innerHTML = "today Duration : " +  todayDuration + "min";
+    //
 }
 
 visitsRef.on("value" , gotVisits , errVisits) ;
